b9e972d6e3c183b0fb36e343d0a1ba0c5bddf918
Piou-piou <pilloud.anthony@gmail.com>
2026-04-09 15:17:20 +0200

    chore: refine PWA configuration and fix manifest loading

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Add support for serving the public directory as static assets
-config.resolver.assetExts.push('json', 'webmanifest');
 
 module.exports = config;
